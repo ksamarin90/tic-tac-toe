60ab4a39b07c6ac2b04e14dd5bedbb548d505d6b
ksamarin <ksamarin90@gmail.com>
2020-04-01 21:43:13 +0300

    Updates

---
+++ precache-manifest.708fef9fa5f33b85a1fe7af521a6ac37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b5dc746f24774ba9198804344dc689",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "75bdaebdab27066322f7",
+    "url": "/tic-tac-toe/static/js/2.f906abf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.f906abf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c122797abfe11eafa60d",
+    "url": "/tic-tac-toe/static/js/main.a9806230.chunk.js"
+  },
+  {
+    "revision": "85cc3683a64921c9f4b4",
+    "url": "/tic-tac-toe/static/js/runtime-main.769e5d30.js"
+  }
+]);
